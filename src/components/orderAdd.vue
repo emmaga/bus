305bd4be6828b112d65b7706357078c7647e081b
Emma <emma_0812@msn.cn>
2017-07-26 18:33:45 +0800

    增加 预订人姓名、手机、人数

--- src/components/orderAdd.vue
+++ src/components/orderAdd.vue
@@ -11,6 +11,10 @@
         @confirm="handleChange">
       </mt-datetime-picker>
     </div>
+    <mt-field label="姓名" placeholder="请输入姓名" v-model="ordername"></mt-field>
+    <mt-field label="手机" placeholder="请输入手机号码" v-model="phonenum" state="warning"></mt-field>
+    <mt-picker :slots="slots2" @change="onValuesChange2"></mt-picker>
+    <mt-button type="primary">提交预约</mt-button>
   </div>
 </template>
 
@@ -28,11 +32,27 @@ export default {
         }
       ],
       value: '2017-07-26 10:00',
-      visible: false
+      visible: false,
+      ordername: '',
+      phonenum: null,
+      slots2: [
+        {
+          flex: 1,
+          values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+          className: 'slot1',
+          textAlign: 'center'
+        }
+      ]
     }
   },
   methods: {
     onValuesChange (picker, values) {
+      if (values[0] > values[1]) {
+        picker.setSlotValue(1, values[0])
+      }
+      console.log(values)
+    },
+    onValuesChange2 (picker, values) {
       if (values[0] > values[1]) {
         picker.setSlotValue(1, values[0])
       }
